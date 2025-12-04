3e3a1e48509781cc82ffbcf4be4698712e6d3300
Enie Weiß <enie@eniestudio.de>
2025-12-04 20:24:24 +0100

    Initial Commit

---
+++ ClearGlassEffect/ClearGlassEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearGlassEffectApp.swift
+//  ClearGlassEffect
+//
+//  Created by Enie Weiß on 04.12.25.
+//
+
+import SwiftUI
+
+@main
+struct ClearGlassEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
